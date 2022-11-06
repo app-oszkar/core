27f937d27ab05dc596ae79c86e453126151484f6
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2022-11-06 20:59:04 +0100

    Update core.js

--- core.js
+++ core.js
@@ -7,37 +7,28 @@
 
 function build_embed_src() {
     // Variables
-    const base_url = 'https://kiwiirc.com/nextclient/';
-	  const plugins = 'conference';
-	  const theme = 'osprey';
-    const server_url = 'irc.radioclick.ro';
-	  const server_port = '6667';
-	  const chan = '#Romania';
-    const nick = 'User??';
-	
+        const base_url = 'https://webchat.romaniachat.eu';
+	const port = '8443';
+	const nick = 'User??';
+	const theme = 'osprey';
+	const chan = 'Romania';
     // Get page URL information
     let url = new URL(window.location);
-
     // Construct the iframe src URL
     let src = base_url;
-	if (plugins) {
-        src += '?plugins=' + plugins;
-    }
-	if (theme) {
-        src += '&theme=' + theme;
-    } 
-	if (server_url) {
-        src += '#irc://' + server_url;
-    }
-	if (server_port) {
-        src += ':' + server_port;
-    } 
-	 if (chan) {
-        src += '/' + chan;
+	if (port) {
+        src += ':' + port + '/';
     }
 	if (nick) {
-        src += '?&nick=' + nick;
+        src += '?' + '&' + 'nick' + '=' + nick;
     }
+	if (theme) {
+        src += '&' + 'theme=' + theme;
+    } 
+	 if (chan) {
+        src += '#' + chan;
+    }
+	
     return src;
 }
 function change_embed_dimensions() {
